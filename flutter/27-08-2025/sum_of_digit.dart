90310a9d276cc28142c39dcb1a057194d5245d7a
jiyahargun <jiyahargun82@gmail.com>
2025-08-28 20:48:25 +0530

    commit

---
+++ flutter/27-08-2025/sum_of_digit.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main()
+{
+  var sum =0;
+
+  print("Enter Any Number");
+  var num = int.parse(stdin.readLineSync().toString());
+
+  while(num > 0)
+  {
+    int rem = num % 10;
+    sum+=rem;
+    num=num ~/ 10;
+
+  }
+  print("Sum of all digits are: $sum");
+}
