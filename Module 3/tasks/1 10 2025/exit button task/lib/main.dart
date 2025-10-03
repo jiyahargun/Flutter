1498b52a363d952315e302845264a896875144b2
jiyahargun <jiyahargun82@gmail.com>
2025-10-03 13:09:20 +0530

    commit

---
+++ Module 3/tasks/1 10 2025/exit button task/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:taskk/dashboard.dart';
+import 'package:taskk/splashscreen.dart';
+
+void main() {
+  runApp(MaterialApp(home: Splashscreen(), debugShowCheckedModeBanner: false));
+}
+
+class FormEx extends StatefulWidget {
+  const FormEx({super.key});
+
+  @override
+  State<FormEx> createState() => _FormExState();
+}
+
+class _FormExState extends State<FormEx> {
+  TextEditingController mob = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  var _formkey = GlobalKey<FormState>();
+  var password = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login Form")),
+      body: Form(
+        key: _formkey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: mob,
+                  decoration: InputDecoration(
+                    labelText: "Enter Mobile",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.00),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter Mobile Number";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: pass,
+                  obscureText: password,
+                  decoration: InputDecoration(
+                    labelText: "Enter Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.00),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        password ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          password = !password;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter Password";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    String a = mob.text.toString();
+                    String b = pass.text.toString();
+
+                    if (_formkey.currentState!.validate()) {
+                      if (a == "1010101010" && b == "1234") {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Logged in Successfully")),
+                        );
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SecondScreen(),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Invalid Credentials")),
+                        );
+                      }
+                    }
+                  },
+                  child: Text("Login"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
