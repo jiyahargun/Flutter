b1435c17ec4fe0ecd4b7c937028ea0904c1e32e2
jiyahargun <jiyahargun82@gmail.com>
2025-10-27 11:45:52 +0530

    commit

--- Assignments/AdvanceUi_Module_4/Advance UI/task31.dart
+++ Assignments/AdvanceUi_Module_4/Advance UI/task31.dart
@@ -1,22 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: CardEx(), debugShowCheckedModeBanner: false));
 }
 
-class MyApp extends StatelessWidget {
+class CardEx extends StatefulWidget {
+  const CardEx({super.key});
+
+  @override
+  State<CardEx> createState() => _CardExState();
+}
+
+class _CardExState extends State<CardEx> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(title: Text("Card")),
       body: Center(
-        child: Text(
-          'Welcome, Jiya!',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
+        child: Stack(
+          children: [
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                width: 220,
+                height: 180,
+                padding: const EdgeInsets.all(16),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Card Example",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("Card Example"),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ),
+          ],
         ),
       ),
     );
