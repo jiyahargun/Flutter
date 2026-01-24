dfc406a0b05f560c8be51458d337b1155be490d4
jiyahargun <jiyahargun82@gmail.com>
2026-01-24 12:10:45 +0530

    commit

--- assessment/tea_diary/teadiary/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'screens/splashscreen.dart' show SplashScreen;
-
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()));
-}
